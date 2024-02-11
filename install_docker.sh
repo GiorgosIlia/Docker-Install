94845403442d98be510987b10ba73f72113a20bd
Giorgos Ilia <giorkos313@gmail.com>
2024-02-11 14:49:15 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,16 +3,19 @@
 # Remove existing Docker-related packages if any
 echo "Removing existing Docker-related packages..."
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
-    sudo apt-get remove -y $pkg
+   sudo apt-get remove $pkg;
 done
+            
 
 # Update package list
 echo "Updating package list..."
 sudo apt-get update
-
+sudo apt-get install ca-certificates curl gnupg
+   
 # Install prerequisites
 echo "Installing prerequisites..."
-sudo apt-get install -y ca-certificates curl gnupg
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
 
 # Create directory for apt keyrings
 echo "Creating directory for apt keyrings..."
@@ -21,14 +24,22 @@ sudo install -m 0755 -d /etc/apt/keyrings
 # Download Docker's official GPG key and store it in the keyring directory
 echo "Downloading Docker's official GPG key..."
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Set appropriate permissions for the GPG key file
 echo "Setting permissions for the GPG key file..."
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+# Set Up the docker Repository 
+echo \
+"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 # Install Docker CE, Docker CLI, Containerd, Docker Buildx Plugin, Docker Compose Plugin
 echo "Installing Docker CE, Docker CLI, Containerd, Docker Buildx Plugin, Docker Compose Plugin..."
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Restart Docker service
 echo "Restarting Docker service..."
